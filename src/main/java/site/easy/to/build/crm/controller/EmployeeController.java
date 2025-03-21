16618503c2834b3f9e7f85a37623f9f855e009d3
sergianafr <sergiravelo@gmail.com>
2025-03-21 13:37:52 +0300

    page import employee

--- src/main/java/site/easy/to/build/crm/controller/EmployeeController.java
+++ src/main/java/site/easy/to/build/crm/controller/EmployeeController.java
@@ -1,17 +1,25 @@
 package site.easy.to.build.crm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import site.easy.to.build.crm.util.AuthenticationUtils;
+
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
+    @Autowired
+    private AuthenticationUtils authenticationUtils;
+
     @GetMapping("/import-page")
-    public String getMethodName(){
-        return "";
+    public String getMethodName(Authentication authentication){
+        int userId = authenticationUtils.getLoggedInUserId(authentication);
+        return "import-page";
     }
     
 }
