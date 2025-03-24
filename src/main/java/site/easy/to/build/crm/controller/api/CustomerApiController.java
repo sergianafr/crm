b4b8d8ff6f80b1ddf2ed924ed28eb814b0b87a28
sergianafr <sergiravelo@gmail.com>
2025-03-24 09:35:16 +0300

    update front alerts

--- src/main/java/site/easy/to/build/crm/controller/api/CustomerApiController.java
+++ src/main/java/site/easy/to/build/crm/controller/api/CustomerApiController.java
@@ -8,6 +8,7 @@ import site.easy.to.build.crm.service.RateService;
 import site.easy.to.build.crm.service.customer.CustomerService;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,29 +26,32 @@ public class CustomerApiController {
     @Autowired private RateService rateService;
     @GetMapping("/checkExpense")
     @CrossOrigin(origins = "http://localhost:8080")
-    public ResponseEntity<String> calculateExpense(@RequestParam Integer customerId,
-    @RequestParam BigDecimal amount) {
+    public ResponseEntity<Map<String, Object>> calculateExpense(@RequestParam Integer customerId,
+                                                            @RequestParam BigDecimal amount) {
         try {
-            // Integer customerId = Integer.parseInt(request.get("customerId").toString());
-            // BigDecimal amount = new BigDecimal(request.get("amount").toString());
             BigDecimal actualExpense = customerService.getTotalExpense(customerId);
-            System.out.println(actualExpense);
             BigDecimal totalBudget = customerService.getTotalBudget(customerId);
-            System.out.println(totalBudget);
             Rate maxRate = rateService.findMax();
             BigDecimal rateValue = totalBudget.multiply(BigDecimal.valueOf(maxRate.getRate()).divide(BigDecimal.valueOf(100)));
-            System.out.println(rateValue);
-            
+
             BigDecimal totalExpenseWithAmount = actualExpense.add(amount);
 
+            Map<String, Object> response = new HashMap<>();
+            response.put("maxRate", maxRate.getRate());
+            response.put("maxValueRate", rateValue);
+            response.put("totalBudget", totalBudget);
+
             if (totalExpenseWithAmount.compareTo(rateValue) <= 0) {
-                return ResponseEntity.ok("valid");
+                response.put("status", "valid");
             } else if (totalExpenseWithAmount.compareTo(rateValue) >= 0 && totalExpenseWithAmount.compareTo(totalBudget) <= 0) {
-                return ResponseEntity.ok("superior");
-            } else if(totalExpenseWithAmount.compareTo(totalBudget) >= 0) {
-                return ResponseEntity.ok("exceed");
+                response.put("status", "superior");
+            } else if (totalExpenseWithAmount.compareTo(totalBudget) >= 0) {
+                response.put("status", "exceed");
+            } else {
+                response.put("status", "valid");
             }
-            return ResponseEntity.ok("valid");
+
+            return ResponseEntity.ok(response);
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().build();
